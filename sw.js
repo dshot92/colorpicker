a8a3688505145e48f6a2955f8078b0362b67d668
dshot92 <dshot92@gmail.com>
2025-06-24 00:43:25 +0200

    activate

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  self.skipWaiting();
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
@@ -15,14 +17,26 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== 'v1').map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
   event.respondWith(
     caches.match(event.request).then(response => {
       if (response) {
         return response;
       }
       return fetch(event.request).catch(error => {
-        // Fallback for navigation requests (e.g., SPA)
         if (event.request.mode === 'navigate') {
           return caches.match('/index.html');
         }
